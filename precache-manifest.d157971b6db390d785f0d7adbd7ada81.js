90250a2df547e3cf93f8fb567a6086c79662587d
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-03-25 19:11:51 +0100

    Updates

---
+++ precache-manifest.d157971b6db390d785f0d7adbd7ada81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298b922d2f619e17496edec05887ee2d",
+    "url": "/frontend-engineer-test/index.html"
+  },
+  {
+    "revision": "529576769275be7290dd",
+    "url": "/frontend-engineer-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "065134ee57f553d677ef",
+    "url": "/frontend-engineer-test/static/js/2.12bfce92.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/frontend-engineer-test/static/js/2.12bfce92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529576769275be7290dd",
+    "url": "/frontend-engineer-test/static/js/main.1abd0fc3.chunk.js"
+  },
+  {
+    "revision": "9bfde3f6f6314a37c3c2",
+    "url": "/frontend-engineer-test/static/js/runtime-main.05ffc87b.js"
+  }
+]);
